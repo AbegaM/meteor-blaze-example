9d8927e88294b3a71f8360c1b9c5e2a56f247015
AbegaM <ebenezermelkamu@gmail.com>
2023-03-17 19:26:23 +0300

    Add reactive variable for update method call

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,14 +1,26 @@
 import { Meteor } from "meteor/meteor";
+import { ReactiveVar } from "meteor/reactive-var";
 import { Template } from "meteor/templating";
 
 import "./Task.html";
 
+Template.task.onCreated(function () {
+  this.isUpdateLoading = new ReactiveVar(false);
+});
+
 Template.task.events({
   "click .toggle-checked"(event) {
+    Template.instance().isUpdateLoading.set(true);
     Meteor.call("tasks.setIsChecked", this._id, !this.isChecked);
-    this.isLoading = false;
+    Template.instance().isUpdateLoading.set(false);
   },
   "click .delete"() {
     Meteor.call("tasks.remove", this._id);
+  },
+});
+
+Template.task.helpers({
+  isUpdateLoading() {
+    return Template.instance().isUpdateLoading.get();
   },
 });
