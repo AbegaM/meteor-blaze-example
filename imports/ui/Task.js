d25efef885b10b49b96b2381d31db1a19dfc16a2
AbegaM <ebenezermelkamu@gmail.com>
2023-03-29 18:38:41 +0300

    fix state error in task event listner + add sleep function

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -4,16 +4,21 @@ import { Template } from "meteor/templating";
 
 import "./Task.html";
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 Template.task.onCreated(function () {
   this.isUpdateLoading = new ReactiveVar(false);
 });
 
 Template.task.events({
-  async "click .toggle-checked"(event) {
+  async "click .toggle-checked"(event, template) {
     try {
-      Template.instance().isUpdateLoading.set(true);
+      template.isUpdateLoading.set(true);
       await Meteor.call("tasks.setIsChecked", this._id, !this.isChecked);
-      Template.instance().isUpdateLoading.set(false);
+      await sleep(3000);
+      template.isUpdateLoading.set(false);
     } catch (error) {
       console.log(error);
     }
